b5875a4250b0d106b6ee95b264abd08158d54cee
Michael Harrington <mike85h@gmail.com>
2017-08-28 10:15:48 -0600

    added bootstrap, fontawesome, modified css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
